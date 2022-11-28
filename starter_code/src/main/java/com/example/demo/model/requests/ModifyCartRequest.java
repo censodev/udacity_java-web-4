28f6b35c6313f869d2901956510e5f94be9cac29
Phuong Bui <buiphuongaptutc@gmail.com>
2022-11-28 10:41:59 +0700

    tests coverage

--- starter_code/src/main/java/com/example/demo/model/requests/ModifyCartRequest.java
+++ starter_code/src/main/java/com/example/demo/model/requests/ModifyCartRequest.java
@@ -1,42 +1,25 @@
 package com.example.demo.model.requests;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ModifyCartRequest {
-	
+
 	@JsonProperty
 	private String username;
-	
+
 	@JsonProperty
 	private long itemId;
-	
+
 	@JsonProperty
 	private int quantity;
-
-	public String getUsername() {
-		return username;
-	}
-
-	public void setUsername(String username) {
-		this.username = username;
-	}
-
-	public long getItemId() {
-		return itemId;
-	}
-
-	public void setItemId(long itemId) {
-		this.itemId = itemId;
-	}
-
-	public int getQuantity() {
-		return quantity;
-	}
-
-	public void setQuantity(int quantity) {
-		this.quantity = quantity;
-	}
-	
-	
-
 }
--- starter_code/src/test/java/com/example/demo/SareetaApplicationTests.java
+++ starter_code/src/test/java/com/example/demo/SareetaApplicationTests.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import com.example.demo.model.persistence.User;
 import com.example.demo.model.requests.CreateUserRequest;
 import com.example.demo.model.requests.LoginRequest;
+import com.example.demo.model.requests.ModifyCartRequest;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.SneakyThrows;
 import org.junit.jupiter.api.Test;
@@ -20,7 +21,6 @@ import java.net.http.HttpResponse;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class SareetaApplicationTests {
@@ -33,7 +33,7 @@ class SareetaApplicationTests {
     private final HttpClient http = HttpClient.newHttpClient();
 
     @Test
-    void createUserThenLogin() throws IOException, InterruptedException {
+    void fullFlow() throws IOException, InterruptedException {
         var createUserRes = createUser("admin", "admin", "admin1");
         assertEquals(400, createUserRes.statusCode());
 
@@ -48,7 +48,40 @@ class SareetaApplicationTests {
 
         loginRes = login("admin", "admin");
         assertEquals(200, loginRes.statusCode());
-        assertTrue(loginRes.headers().firstValue(HttpHeaders.AUTHORIZATION).isPresent());
+        var token = loginRes.headers().firstValue(HttpHeaders.AUTHORIZATION).orElse(null);
+        assertNotNull(token);
+
+        var pushCartRes = pushCart("admin", 1, 1, null);
+        assertEquals(401, pushCartRes.statusCode());
+        pushCartRes = pushCart("admin1", 1, 1, token);
+        assertEquals(404, pushCartRes.statusCode());
+        pushCartRes = pushCart("admin", 3, 1, token);
+        assertEquals(404, pushCartRes.statusCode());
+        pushCartRes = pushCart("admin", 1, 1, token);
+        assertEquals(200, pushCartRes.statusCode());
+
+        var popCartRes = popCart("admin", 1, 1, null);
+        assertEquals(401, popCartRes.statusCode());
+        popCartRes = popCart("admin1", 1, 1, token);
+        assertEquals(404, popCartRes.statusCode());
+        popCartRes = popCart("admin", 3, 1, token);
+        assertEquals(404, popCartRes.statusCode());
+        popCartRes = popCart("admin", 1, 1, token);
+        assertEquals(200, popCartRes.statusCode());
+
+        var orderRes = order("admin", null);
+        assertEquals(401, orderRes.statusCode());
+        orderRes = order("admin1", token);
+        assertEquals(404, orderRes.statusCode());
+        orderRes = order("admin", token);
+        assertEquals(200, orderRes.statusCode());
+
+        var orderHistoryRes = orderHistory("admin", null);
+        assertEquals(401, orderHistoryRes.statusCode());
+        orderHistoryRes = order("admin1", token);
+        assertEquals(404, orderHistoryRes.statusCode());
+        orderHistoryRes = order("admin", token);
+        assertEquals(200, orderHistoryRes.statusCode());
     }
 
     private URI uri(String path) {
@@ -83,5 +116,50 @@ class SareetaApplicationTests {
                 .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
                 .build();
         return http.send(req, HttpResponse.BodyHandlers.discarding());
+    }
+
+    private HttpResponse<String> pushCart(String username, long itemId, int qty, String token) throws IOException, InterruptedException {
+        var body = json(ModifyCartRequest.builder()
+                .username(username)
+                .itemId(itemId)
+                .quantity(qty)
+                .build());
+        var req = HttpRequest.newBuilder(uri("/api/cart/addToCart"))
+                .POST(HttpRequest.BodyPublishers.ofString(body))
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .header(HttpHeaders.AUTHORIZATION, "Bearer " + token)
+                .build();
+        return http.send(req, HttpResponse.BodyHandlers.ofString());
+    }
+
+    private HttpResponse<String> popCart(String username, long itemId, int qty, String token) throws IOException, InterruptedException {
+        var body = json(ModifyCartRequest.builder()
+                .username(username)
+                .itemId(itemId)
+                .quantity(qty)
+                .build());
+        var req = HttpRequest.newBuilder(uri("/api/cart/removeFromCart"))
+                .POST(HttpRequest.BodyPublishers.ofString(body))
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .header(HttpHeaders.AUTHORIZATION, "Bearer " + token)
+                .build();
+        return http.send(req, HttpResponse.BodyHandlers.ofString());
+    }
+
+    private HttpResponse<String> order(String usn, String token) throws IOException, InterruptedException {
+        var req = HttpRequest.newBuilder(uri("/api/order/submit/" + usn))
+                .POST(HttpRequest.BodyPublishers.noBody())
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .header(HttpHeaders.AUTHORIZATION, "Bearer " + token)
+                .build();
+        return http.send(req, HttpResponse.BodyHandlers.ofString());
+    }
+
+    private HttpResponse<String> orderHistory(String usn, String token) throws IOException, InterruptedException {
+        var req = HttpRequest.newBuilder(uri("/api/order/history/" + usn))
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .header(HttpHeaders.AUTHORIZATION, "Bearer " + token)
+                .build();
+        return http.send(req, HttpResponse.BodyHandlers.ofString());
     }
 }
