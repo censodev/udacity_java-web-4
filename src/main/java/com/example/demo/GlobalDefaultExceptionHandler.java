22ac276b3a55d1f0f678bacadec22ea255a12902
Phuong Bui <buiphuongaptutc@gmail.com>
2022-11-29 21:47:39 +0700

    splunk results

--- src/main/java/com/example/demo/GlobalDefaultExceptionHandler.java
+++ src/main/java/com/example/demo/GlobalDefaultExceptionHandler.java
@@ -8,7 +8,8 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @Slf4j
 public class GlobalDefaultExceptionHandler {
     @ExceptionHandler(Exception.class)
-    public void handle(Exception ex) {
+    public void handle(Exception ex) throws Exception {
         log.error(ex.getMessage());
+        throw ex;
     }
 }
